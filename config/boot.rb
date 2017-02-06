4905722a9b7b725cff3c4d1c52636aba7d9ef553
liao <461389537@qq.com>
2017-02-06 10:52:49 +0800

    shutdown

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,15 @@
+
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require 'rails/commands/server'
+
+module Rails
+  class Server
+    def default_options
+      super.merge({Port: 8000, Host: '10.19.5.237'})
+    end
+  end
+end
